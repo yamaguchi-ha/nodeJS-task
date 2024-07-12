95b9fa0e31e7d023a7c9cbe733c61e20c1d1db3a
harunayamaguchi <haruportfolio6151@gmail.com>
2024-07-12 17:35:42 +0900

    課題1,2の対応

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
-const path = require('path');
-const express = require('express');
-const ejs = require('ejs');
+const path = require("path");
+const express = require("express");
+const ejs = require("ejs");
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const con = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'rootroot',
-  database: 'express_db'
+  host: "localhost",
+  user: "root",
+  password: "rootroot",
+  database: "express_db",
 });
 
 // mysqlからデータを持ってくる
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const sql = "select * from users";
   // 参考例
   const num = 10000;
+  const food = "にんじん";
+  const pet = ["犬", "猫", "うさぎ", "鳥", "カメ"];
+  const member = [
+    {
+      name: "s.chiba",
+      email: "s.chiba@gmail.com",
+    },
+    {
+      name: "t.kosuge",
+      email: "t.kosuge@gmail.com",
+    },
+    {
+      name: "m.chiba",
+      email: "m.chiba@gmail.com",
+    },
+    {
+      name: "t.suzuki",
+      email: "t.suzuki@gmail.com",
+    },
+    {
+      name: "t.hasegawa",
+      email: "t.hasegawa@gmail.com",
+    },
+  ];
+
   // 基礎課題項目
   /* ==========従来通りJavaScriptの要領で書いてください。==========
     ここで記載する内容はブラウザに出力するための情報のみになります。上記参考例のconst num = 10000;のように
@@ -43,46 +68,49 @@ app.get('/', (req, res) => {
   // ==========ここまでの範囲で書くようにしましょう。==========
   con.query(sql, function (err, result, fields) {
     if (err) throw err;
-    res.render('index', {
+    res.render("index", {
       users: result,
+      // ↑のusers: resultを記述することで、mysql内のデータを引っ張ってきている。
+
       // ⓵ こちらはapp.jsで宣言した変数をindex.ejsのscriptタグ内で使用するために登録する場所になっています。
       /*
         指定の仕方はオブジェクトの考え方と同じで、プロパティ名: 値の形になります。値の部分は変数名を入れるようにして下さい。
         プロパティ名はindex.ejsで使用しますので、何の値が入ってるかわかるような名前にしましょう。
       */
-      number: num
+      number: num,
+      food: food,
+      pet: pet,
+      member: member,
     });
   });
 });
 
-app.get('/edit/:id', (req, res) => {
+app.get("/edit/:id", (req, res) => {
   const sql = "SELECT * FROM users WHERE id = ?";
   con.query(sql, [req.params.id], function (err, result, fields) {
     if (err) throw err;
-    res.render('edit', {
-      user: result
+    res.render("edit", {
+      user: result,
     });
   });
 });
 
-app.post('/update/:id', (req, res) => {
+app.post("/update/:id", (req, res) => {
   const sql = "UPDATE users SET ? WHERE id = " + req.params.id;
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
-    res.redirect('/');
+    res.redirect("/");
   });
 });
 
-app.get('/delete/:id', (req, res) => {
+app.get("/delete/:id", (req, res) => {
   const sql = "DELETE FROM users WHERE id = ?";
-  con.query(
-    sql, [req.params.id],
-    function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      res.redirect('/');
-    });
+  con.query(sql, [req.params.id], function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    res.redirect("/");
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
